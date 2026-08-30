dcf37983b02c654accbbcfa7f297916d843a3e85
msalman-amjad <salmanamjad915@gmail.com>
2026-08-30 21:46:41 +0500

    feat: complete 07reactRouter project

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-300 p-4'>react router</h1>
+
+    </>
+  )
+}
+
+export default App
